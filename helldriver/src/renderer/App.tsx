951034f358ad6974e972b3b3d3e83e069ce5e8a5
Marcel Raszewski <112889734+tenktopodpalil@users.noreply.github.com>
2026-03-19 12:56:29 +0100

    added Super Earth

--- helldriver/src/renderer/App.tsx
+++ helldriver/src/renderer/App.tsx
@@ -21,48 +21,7 @@ async function fetchAPIData(endpoint: string) {
   return data;
   
 }
-function Hello() {
-  const [text,setText] = useState<string>('');
-// renderer.js
 
-
-fetchAPIData(text);
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚{text}
-            </span>
-            
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate 
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
-//
 
 function MapComponent() {
   
@@ -118,41 +77,25 @@ mapRef.current = new maplibregl.Map({
   ]
 });
 // Add the point after the map loads
+mapRef.current.on('load', async () => {
+  const map = mapRef.current!;
 
+
+});
 mapRef.current.on('load', async () => {
   const map = mapRef.current!;
 
   const image = await map.loadImage(Super_Ziemia);
   map.addImage('custom-marker', image.data);
 
-  map.addSource('point', {
-    type: 'geojson',
-    data: {
-      type: 'FeatureCollection',
-      features: [
-        {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [0.5, 0.5] // 👈 replace with your [lng, lat]
-          },
-          properties: {}
-        }
-      ]
-    }
-  });
+  const el = document.createElement('img');
+  el.src = Super_Ziemia;
+  el.style.width = '32px';  
+  el.style.height = '32px';
 
-  map.addLayer({
-    id: 'point',
-    type: 'symbol',
-    source: 'point',
-    layout: {
-      'icon-image': 'custom-marker',
-      'icon-size': 1,
-      'icon-anchor': 'center',
-      'icon-allow-overlap': true
-    }
-  });
+  new maplibregl.Marker({ element: el })
+    .setLngLat([0.5, 0.5]) 
+    .addTo(map);
 });
     
     return () => {
